17c62ac37b32efcd89b3fdfdc1742a0735adeecd
kimkihyeon <kimki1125@naver.com>
2025-07-19 22:55:34 +0900

    [feat] 식단 생성 화면에서 쓸 TitledContainerView 생성

--- BalanceEat/Presentation/Create/CreateViewController.swift
+++ BalanceEat/Presentation/Create/CreateViewController.swift
@@ -9,19 +9,20 @@ import UIKit
 import SnapKit
 
 class CreateViewController: UIViewController {
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    
+    private let vvvv: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    private lazy var titledContainerView = TitledContainerView(title: "타이트르를", contentView: vvvv)
+    
     init() {
         super.init(nibName: nil, bundle: nil)
         
-        let label = UILabel()
-        label.text = "추가"
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        
-        view.addSubview(label)
-        
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
+        setUpView()
     }
     
     required init?(coder: NSCoder) {
@@ -30,5 +31,26 @@ class CreateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    private func setUpView() {
+        view.backgroundColor = .homeScreenBackground
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(titledContainerView)
+        
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(scrollView.snp.width)
+        }
+        
+        titledContainerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(300)
+        }
     }
 }
---
+++ BalanceEat/Presentation/Create/TitledContainerView.swift
@@ -0,0 +1,56 @@
+//
+//  TitledContainerView.swift
+//  BalanceEat
+//
+//  Created by 김견 on 7/17/25.
+//
+
+import UIKit
+import SnapKit
+
+final class TitledContainerView: UIView {
+    private let title: String
+    private let backgroundView = HomeMenuContentView()
+    private let contentView: UIView
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        label.textColor = .bodyStatusCardNumber
+        return label
+    }()
+    
+    init(title: String, contentView: UIView) {
+        self.title = title
+        self.contentView = contentView
+        super.init(frame: .zero)
+        
+        setUpView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpView() {
+        self.addSubview(backgroundView)
+        backgroundView.addSubview(titleLabel)
+        backgroundView.addSubview(contentView)
+        
+        titleLabel.text = title
+        
+        backgroundView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().offset(20)
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(12)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.bottom.equalToSuperview().offset(-20)
+        }
+    }
+}
