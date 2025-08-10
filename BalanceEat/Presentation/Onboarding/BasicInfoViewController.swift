dee8b62d910106ca65299b20fa66d1994d122c24
kimkihyeon <kimki1125@naver.com>
2025-08-10 21:37:39 +0900

    [chore] 성별 선택 버튼 색 변경

--- BalanceEat/Presentation/Onboarding/BasicInfoViewController.swift
+++ BalanceEat/Presentation/Onboarding/BasicInfoViewController.swift
@@ -81,11 +81,11 @@ class BasicInfoViewController: UIViewController {
         
         let maleButton = SelectableTitledButton(
             title: "남성",
-            style: .init(backgroundColor: .white, titleColor: .black, borderColor: .gray)
+            style: .init(backgroundColor: .white, titleColor: .black, borderColor: .lightGray.withAlphaComponent(0.4))
         )
         let femaleButton = SelectableTitledButton(
             title: "여성",
-            style: .init(backgroundColor: .white, titleColor: .black, borderColor: .gray)
+            style: .init(backgroundColor: .white, titleColor: .black, borderColor: .lightGray.withAlphaComponent(0.4))
         )
         let genderButtons = [maleButton, femaleButton]
         
@@ -138,47 +138,6 @@ class BasicInfoViewController: UIViewController {
         [genderInputView, ageInputView, heightInputView, weightInputView, nextButton].forEach {
             mainStackView.addArrangedSubview($0)
         }
-    }
-}
-
-final class TitledInputUserInfoView: UIView {
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 14, weight: .medium)
-        label.textColor = .label
-        return label
-    }()
-    
-    private let contentView: UIView
-    
-    private let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 8
-        return stackView
-    }()
-    
-    init(title: String, inputView: UIView) {
-        self.contentView = inputView
-        super.init(frame: .zero)
-        
-        titleLabel.text = title
-        setUpView()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setUpView() {
-        addSubview(stackView)
-        
-        stackView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        stackView.addArrangedSubview(titleLabel)
-        stackView.addArrangedSubview(contentView)
     }
 }
 
