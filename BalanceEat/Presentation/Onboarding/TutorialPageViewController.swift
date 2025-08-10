d29ad8a8bd1618f2ecf71276d78d0fe26d076922
kimkihyeon <kimki1125@naver.com>
2025-08-10 18:22:30 +0900

    [chore] 튜토리얼 화면에서 화면 움직임 애니메이션 추가

--- BalanceEat/Presentation/Onboarding/TutorialPageViewController.swift
+++ BalanceEat/Presentation/Onboarding/TutorialPageViewController.swift
@@ -15,6 +15,7 @@ class TutorialPageViewController: UIViewController {
     
     private var pages: [UIViewController] = []
     private var currentIndex: Int = 0
+    private var previousIndex: Int = 0
     
     private let disposeBag = DisposeBag()
     
@@ -42,34 +43,53 @@ class TutorialPageViewController: UIViewController {
     private func displayCurrentPage(animated: Bool) {
         if let currentChild = children.first {
             currentChild.willMove(toParent: nil)
-            currentChild.view.removeFromSuperview()
-            currentChild.removeFromParent()
+            if animated {
+                let direction: CGFloat = (currentIndex > previousIndex) ? 1 : -1
+                UIView.animate(withDuration: 0.3, animations: {
+                    currentChild.view.frame.origin.x = -direction * self.view.bounds.width
+                }, completion: { _ in
+                    currentChild.view.removeFromSuperview()
+                    currentChild.removeFromParent()
+                })
+            } else {
+                currentChild.view.removeFromSuperview()
+                currentChild.removeFromParent()
+            }
         }
         
         let vc = pages[currentIndex]
         addChild(vc)
-        vc.view.frame = view.bounds
-        view.addSubview(vc.view)
-        vc.didMove(toParent: self)
+        if animated {
+            let direction: CGFloat = (currentIndex > previousIndex) ? 1 : -1
+            let startX = direction * view.bounds.width
+            vc.view.frame = view.bounds.offsetBy(dx: startX, dy: 0)
+            view.addSubview(vc.view)
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                vc.view.frame = self.view.bounds
+            }, completion: { _ in
+                vc.didMove(toParent: self)
+            })
+        } else {
+            vc.view.frame = view.bounds
+            view.addSubview(vc.view)
+            vc.didMove(toParent: self)
+        }
         
         currentPageRelay.accept((currentIndex: currentIndex, totalPages: pages.count))
-        
-        if animated {
-            vc.view.alpha = 0
-            UIView.animate(withDuration: 0.3) {
-                vc.view.alpha = 1
-            }
-        }
+        previousIndex = currentIndex
     }
     
     func goToNextPage() {
         guard currentIndex + 1 < pages.count else { return }
+        previousIndex = currentIndex
         currentIndex += 1
         displayCurrentPage(animated: true)
     }
     
     func goToPreviousPage() {
         guard currentIndex - 1 >= 0 else { return }
+        previousIndex = currentIndex
         currentIndex -= 1
         displayCurrentPage(animated: true)
     }
