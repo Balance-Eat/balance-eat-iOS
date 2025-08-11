70162a20fd1d673dd9f39e61635e16808e3492fa
kimkihyeon <kimki1125@naver.com>
2025-08-11 21:13:11 +0900

    [chore] 튜토리얼 화면 넘어갈 때 애니메이션 제거

---
+++ BalanceEat/Presentation/Onboarding/ActivityLevelViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityLevelViewController.swift
+//  BalanceEat
+//
+//  Created by 김견 on 8/11/25.
+//
+
+import Foundation
--- BalanceEat/Presentation/Onboarding/TutorialPageViewController.swift
+++ BalanceEat/Presentation/Onboarding/TutorialPageViewController.swift
@@ -45,46 +45,32 @@ class TutorialPageViewController: UIViewController {
             })
             .disposed(by: disposeBag)
         
+        let activityLevelViewController = ActivityLevelViewController()
+        
+        activityLevelViewController.inputCompleted
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.goToNextPage()
+            })
+            .disposed(by: disposeBag)
+        
         pages.append(basicInfoViewController)
         pages.append(targetInfoViewController)
+        pages.append(activityLevelViewController)
     }
     
     private func displayCurrentPage(animated: Bool) {
         if let currentChild = children.first {
             currentChild.willMove(toParent: nil)
-            if animated {
-                let direction: CGFloat = (currentIndex > previousIndex) ? 1 : -1
-                UIView.animate(withDuration: 0.3, animations: {
-                    currentChild.view.frame.origin.x = -direction * self.view.bounds.width
-                }, completion: { _ in
-                    currentChild.view.removeFromSuperview()
-                    currentChild.removeFromParent()
-                })
-            } else {
-                currentChild.view.removeFromSuperview()
-                currentChild.removeFromParent()
-            }
+            currentChild.view.removeFromSuperview()
+            currentChild.removeFromParent()
         }
         
         let vc = pages[currentIndex]
         addChild(vc)
-        
-        if animated && currentIndex > 1 {
-            let direction: CGFloat = (currentIndex > previousIndex) ? 1 : -1
-            let startX = direction * view.bounds.width
-            vc.view.frame = view.bounds.offsetBy(dx: startX, dy: 0)
-            view.addSubview(vc.view)
-            
-            UIView.animate(withDuration: 0.3, animations: {
-                vc.view.frame = self.view.bounds
-            }, completion: { _ in
-                vc.didMove(toParent: self)
-            })
-        } else {
-            vc.view.frame = view.bounds
-            view.addSubview(vc.view)
-            vc.didMove(toParent: self)
-        }
+        vc.view.frame = view.bounds
+        view.addSubview(vc.view)
+        vc.didMove(toParent: self)
         
         vc.view.snp.makeConstraints { make in
             make.edges.equalToSuperview()
