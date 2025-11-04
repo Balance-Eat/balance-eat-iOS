d4ec467f40e84b98f7f97e24c912d919f914b67f
kimkihyeon <kimki1125@naver.com>
2025-11-04 21:42:53 +0900

    [chore] 통계 mock 데이터 주석처리

--- BalanceEat/Presentation/Main/Chart/ChartViewModel.swift
+++ BalanceEat/Presentation/Main/Chart/ChartViewModel.swift
@@ -80,72 +80,72 @@ final class ChartViewModel: BaseViewModel {
         
         
         
-        currentStatsRelay.accept(
-            [
-                StatsData(
-                    type: .daily,
-                    date: "2025-08-01",
-                    totalCalories: 2000,
-                    totalCarbohydrates: 250,
-                    totalProtein: 100,
-                    totalFat: 50,
-                    weight: 90
-                ),
-                StatsData(
-                    type: .daily,
-                    date: "2025-08-02",
-                    totalCalories: 2100,
-                    totalCarbohydrates: 250,
-                    totalProtein: 120,
-                    totalFat: 50,
-                    weight: 90
-                ),
-                StatsData(
-                    type: .daily,
-                    date: "2025-08-03",
-                    totalCalories: 2300,
-                    totalCarbohydrates: 230,
-                    totalProtein: 220,
-                    totalFat: 70,
-                    weight: 95
-                ),
-                StatsData(
-                    type: .daily,
-                    date: "2025-08-04",
-                    totalCalories: 2600,
-                    totalCarbohydrates: 280,
-                    totalProtein: 100,
-                    totalFat: 60,
-                    weight: 100
-                ),
-                StatsData(
-                    type: .daily,
-                    date: "2025-08-05",
-                    totalCalories: 2100,
-                    totalCarbohydrates: 210,
-                    totalProtein: 50,
-                    totalFat: 50,
-                    weight: 98
-                ),
-                StatsData(
-                    type: .daily,
-                    date: "2025-08-06",
-                    totalCalories: 2450,
-                    totalCarbohydrates: 240,
-                    totalProtein: 80,
-                    totalFat: 90,
-                    weight: 91
-                ),
-                StatsData(
-                    type: .daily,
-                    date: "2025-08-07",
-                    totalCalories: 2760,
-                    totalCarbohydrates: 260,
-                    totalProtein: 135,
-                    totalFat: 239,
-                    weight: 94
-                )
-            ]
-        )
+//        currentStatsRelay.accept(
+//            [
+//                StatsData(
+//                    type: .daily,
+//                    date: "2025-08-01",
+//                    totalCalories: 2000,
+//                    totalCarbohydrates: 250,
+//                    totalProtein: 100,
+//                    totalFat: 50,
+//                    weight: 90
+//                ),
+//                StatsData(
+//                    type: .daily,
+//                    date: "2025-08-02",
+//                    totalCalories: 2100,
+//                    totalCarbohydrates: 250,
+//                    totalProtein: 120,
+//                    totalFat: 50,
+//                    weight: 90
+//                ),
+//                StatsData(
+//                    type: .daily,
+//                    date: "2025-08-03",
+//                    totalCalories: 2300,
+//                    totalCarbohydrates: 230,
+//                    totalProtein: 220,
+//                    totalFat: 70,
+//                    weight: 95
+//                ),
+//                StatsData(
+//                    type: .daily,
+//                    date: "2025-08-04",
+//                    totalCalories: 2600,
+//                    totalCarbohydrates: 280,
+//                    totalProtein: 100,
+//                    totalFat: 60,
+//                    weight: 100
+//                ),
+//                StatsData(
+//                    type: .daily,
+//                    date: "2025-08-05",
+//                    totalCalories: 2100,
+//                    totalCarbohydrates: 210,
+//                    totalProtein: 50,
+//                    totalFat: 50,
+//                    weight: 98
+//                ),
+//                StatsData(
+//                    type: .daily,
+//                    date: "2025-08-06",
+//                    totalCalories: 2450,
+//                    totalCarbohydrates: 240,
+//                    totalProtein: 80,
+//                    totalFat: 90,
+//                    weight: 91
+//                ),
+//                StatsData(
+//                    type: .daily,
+//                    date: "2025-08-07",
+//                    totalCalories: 2760,
+//                    totalCarbohydrates: 260,
+//                    totalProtein: 135,
+//                    totalFat: 239,
+//                    weight: 94
+//                )
+//            ]
+//        )
     }
 }
